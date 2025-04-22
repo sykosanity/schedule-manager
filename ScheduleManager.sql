b07b9adbde82ebf729695f6b8984428c6e7a3e4e
pierceThrough <keeganliboon021106@gmail.com>
2025-04-22 21:58:59 +0800

    Update ScheduleManager.sql

--- ScheduleManager.sql
+++ ScheduleManager.sql
@@ -1,8 +1,8 @@
-accounts-- --------------------------------------------------------
+-- --------------------------------------------------------
 -- Host:                         127.0.0.1
 -- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             12.8.0.6908
+-- HeidiSQL Version:             12.10.0.7000
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -14,28 +14,9 @@ accounts-- --------------------------------------------------------
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- Data exporting was unselected.
 
--- Dumping database structure for schedule_manager_db
-CREATE DATABASE IF NOT EXISTS `schedule_manager_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `schedule_manager_db`;
-
--- Dumping structure for table schedule_manager_db.accounts
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` longtext DEFAULT NULL,
-  `user_password` longtext DEFAULT NULL,
-  `full_name` longtext DEFAULT NULL,
-  `rank` longtext DEFAULT NULL,
-  `permissions` longtext DEFAULT 'user',
-  PRIMARY KEY (`uid`),
-  KEY `uid` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Dumping data for table schedule_manager_db.accounts: ~3 rows (approximately)
-INSERT INTO `accounts` (`uid`, `user_name`, `user_password`, `full_name`, `rank`, `permissions`) VALUES
-	(1, 'Bien', '12345', 'Bien Niño Enric N. Iligan', 'Dean', 'user'),
-	(2, 'Keegan', '12345', 'Keegan Jeoff G. Liboon', 'Professor', 'user'),
-	(3, 'JB', '12345', 'John Benedict B. Bancruz', 'Teacher', 'user');
+-- Data exporting was unselected.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
