4a2bd434ac7220360d34d1c036db009ea54d6d37
pierceThrough <keeganliboon021106@gmail.com>
2025-04-23 18:09:58 +0800

    Update ScheduleManager.sql

--- ScheduleManager.sql
+++ ScheduleManager.sql
@@ -14,12 +14,37 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Dumping data for table schedule_manager_db.accounts: ~4 rows (approximately)
+
+-- Dumping database structure for schedule_manager_db
+CREATE DATABASE IF NOT EXISTS `schedule_manager_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `schedule_manager_db`;
+
+-- Dumping structure for table schedule_manager_db.accounts
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` longtext DEFAULT NULL,
+  `user_password` longtext DEFAULT NULL,
+  `full_name` longtext DEFAULT NULL,
+  `rank` longtext DEFAULT NULL,
+  `permissions` longtext DEFAULT 'user',
+  PRIMARY KEY (`uid`),
+  KEY `uid` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table schedule_manager_db.accounts: ~3 rows (approximately)
 INSERT INTO `accounts` (`uid`, `user_name`, `user_password`, `full_name`, `rank`, `permissions`) VALUES
 	(1, 'Bien', '12345', 'Bien Niño Enric N. Iligan', 'Dean', 'user'),
 	(2, 'Keegan', '12345', 'Keegan Jeoff G. Liboon', 'Professor', 'user'),
 	(3, 'JB', '12345', 'John Benedict B. Bancruz', 'Teacher', 'user'),
 	(15, 'tester', 'test', 'test update', 'tester1', 'Basic');
+
+-- Dumping structure for table schedule_manager_db.courses
+CREATE TABLE IF NOT EXISTS `courses` (
+  `course_code` longtext DEFAULT NULL,
+  `course_title` longtext DEFAULT NULL,
+  `course_type` longtext DEFAULT NULL,
+  `units` double NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table schedule_manager_db.courses: ~8 rows (approximately)
 INSERT INTO `courses` (`course_code`, `course_title`, `course_type`, `units`) VALUES
